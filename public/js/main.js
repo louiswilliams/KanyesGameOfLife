fdd571c10ab469c1f0347699382953ef3388db5e
Alex Bertrand <alex.bertrand13@gmail.com>
2014-09-20 15:14:51 -0400

    MOAR SPARKLES

--- public/js/main.js
+++ public/js/main.js
@@ -11,5 +11,50 @@ function mapCoordToPixel(latitude, longitude) {
     }
 }
 
-var chicago = mapCoordToPixel(41.764391, -87.784413);
-console.log(chicago);
+//var chicago = mapCoordToPixel(41.764391, -87.784413);
+//console.log(chicago);
+
+$(document).ready(function() {
+			console.log("here");
+
+			var dotsToPlot = 
+			[[41.764391, -87.784413],
+			[40.762193, -73.588426],
+			[37.128000, -121.956873],
+			[33.776528, -84.396686]];
+
+			var points = d3.select("#canvas")
+				.selectAll("g")
+				.data(dotsToPlot)
+				.enter().append("g")
+				.attr("class", "ping")
+				.attr("transform", function(d) {
+					var coord = mapCoordToPixel(d[0], d[1]);
+					console.log(coord);
+					return "translate(" + coord["x"] + "," + coord["y"] + ")";
+					return "translate(10,10)";
+				});
+
+			var glow = points.append("circle")
+				.attr("class", "glow")
+				.attr("r", "7");
+			
+			glow.append("animate")
+				.attr("attributeType", "XML")
+				.attr("attributeName", "r")
+				.attr("from", "7")
+				.attr("to", "10")
+				.attr("dur", "1s")
+				.attr("fill", "freeze");
+			glow.append("animate")
+				.attr("attributeType", "CSS")
+				.attr("attributeName", "stroke-opacity")
+				.attr("from", "1")
+				.attr("to", "0")
+				.attr("dur", "1s")
+				.attr("fill", "freeze");
+
+			var dot = points.append("circle")
+				.attr("r", "6")
+				.attr("fill", "url(#dotfill)");
+		});
