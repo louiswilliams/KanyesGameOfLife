9fad57da67612ddb88ec48ee349a4d7a3b4b05ff
Christian Battaglia <christian.d.battaglia@gmail.com>
2014-09-20 16:35:46 -0400

    Twitter REST api working

--- server.js
+++ server.js
@@ -2,19 +2,51 @@
  * Module dependencies.
  */
 var express = require('express');
+var cookieParser = require('cookie-parser');
+var compress = require('compression');
+var session = require('express-session');
 var bodyParser = require('body-parser');
-var path = require('path');
 var logger = require('morgan');
+var csrf = require('lusca').csrf();
+var methodOverride = require('method-override');
+
+var _ = require('lodash');
+var MongoStore = require('connect-mongo')({ session: session });
+var flash = require('express-flash');
+var path = require('path');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var expressValidator = require('express-validator');
+var connectAssets = require('connect-assets');
 
 /**
  * Controllers (route handlers).
  */
 var homeController = require('./controllers/home');
+var userController = require('./controllers/user');
 
 /**
  * API keys and Passport configuration.
  */
-// nothing yet
+var secrets = require('./config/secrets');
+var passportConf = require('./config/passport');
+
+/**
+ * Connect to MongoDB.
+ */
+mongoose.connect(secrets.db);
+mongoose.connection.on('error', function() {
+    console.error('MongoDB Connection Error. Make sure MongoDB is running.');
+});
+
+var hour = 3600000;
+var day = hour * 24;
+var week = day * 7;
+
+/**
+ * CSRF whitelist.
+ */
+var csrfExclude = ['/url1', '/url2'];
 
 /**
  * Create Express server.
@@ -33,10 +65,47 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(compress());
+app.use(connectAssets({
+    paths: ['public/css', 'public/js'],
+    helperContext: app.locals
+}));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded());
+app.use(expressValidator());
+app.use(methodOverride());
+app.use(cookieParser());
+app.use(session({
+    secret: secrets.sessionSecret,
+    store: new MongoStore({
+        url: secrets.db,
+        auto_reconnect: true
+    })
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use(function(req, res, next) {
+    // CSRF protection.
+    if (_.contains(csrfExclude, req.path)) return next();
+    csrf(req, res, next);
+});
+app.use(function(req, res, next) {
+    // Make user object available in templates.
+    res.locals.user = req.user;
+    next();
+});
+//app.use(function(req, res, next) {
+//    // Remember original destination before login.
+//    var path = req.path.split('/')[1];
+//    if (/auth|login|logout|signup|img|fonts|favicon/i.test(path)) {
+//        return next();
+//    }
+//    req.session.returnTo = req.path;
+//    next();
+//});
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: week }));
 
 //io.on('connection', function(socket){
 //   console.log("connection!");
@@ -45,7 +114,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 /**
  * Main routes.
  */
-app.get('/', homeController.index);
+app.get('/', passportConf.isAuthenticated, homeController.index);
+app.get('/login', userController.getLogin);
+app.post('/login', userController.postLogin);
+app.get('/logout', userController.logout);
+app.get('/forgot', userController.getForgot);
+app.post('/forgot', userController.postForgot);
+app.get('/reset/:token', userController.getReset);
+app.post('/reset/:token', userController.postReset);
+app.get('/signup', userController.getSignup);
+app.post('/signup', userController.postSignup);
+app.get('/account', passportConf.isAuthenticated, userController.getAccount);
+app.post('/account/profile', passportConf.isAuthenticated, userController.postUpdateProfile);
+app.post('/account/password', passportConf.isAuthenticated, userController.postUpdatePassword);
+app.post('/account/delete', passportConf.isAuthenticated, userController.postDeleteAccount);
+app.get('/account/unlink/:provider', passportConf.isAuthenticated, userController.getOauthUnlink);
+
+/**
+ * OAuth sign-in routes.
+ */
+app.get('/auth/twitter', passport.authenticate('twitter'));
+app.get('/auth/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/login' }), function(req, res) {
+    res.redirect(req.session.returnTo || '/');
+});
 
 /**
  * Start Express server.
